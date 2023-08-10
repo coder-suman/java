5e1586479a37f2a0fb211c7aa78303e249bea4bf
coder-suman <128512864+coder-suman@users.noreply.github.com>
2023-08-10 22:39:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main{
+    public static void main(String[] args){
+
+        System.out.print("the sum of three number is: ");
+
+        int num1 = 9;
+        int num2 = 4;
+        int num3 = 5;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+
+
+    }
+}
